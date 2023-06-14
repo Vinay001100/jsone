99f0707e76f117a2e52a672c884fe7f08bf8e435
Vinay001100 <chaturrajasekaran@gmail.com>
2023-06-14 19:41:02 +0530

    Local Scope and Function

---
+++ 11.js
@@ -0,0 +1,27 @@
+function myLocalScope() {
+  // Only change code below this line
+ const myVar = 8;
+  console.log('inside myLocalScope', myVar);
+}
+myLocalScope();
+
+// Run and check the console
+// myVar is not defined outside of myLocalScope
+console.log('outside myLocalScope', myVar);
+
+/**
+Local Scope and Functions
+Variables which are declared within a function, as well as the function parameters, have local scope. That means they are only visible within that function.
+
+Here is a function myTest with a local variable called loc.
+
+function myTest() {
+  const loc = "foo";
+  console.log(loc);
+}
+
+myTest();
+console.log(loc);
+The myTest() function call will display the string foo in the console. The console.log(loc) line (outside of the myTest function) will throw an error, as loc is not defined outside of the function. 
+
+ */
